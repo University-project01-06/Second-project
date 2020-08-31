fd68f6042dfc78c46058ca1cd48550e091ae632c
Yaroslav Tsybukh <yaroslavcybuh26092001@gmail.com>
2020-08-31 20:33:03 +0300

    almost full version site

--- js/main.js
+++ js/main.js
@@ -7,6 +7,14 @@ $(function(){
 			$('body,html').animate({scrollTop: top}, 1500);
 		});
 	});
+	$(document).ready(function(){
+		$("#footer").on("click","a", function (event) {
+			event.preventDefault();
+			var id  = $(this).attr('href'),
+				top = $(id).offset().top;
+			$('body,html').animate({scrollTop: top}, 1500);
+		});
+	});
 });
 
 var menuBtn = document.querySelector('.menu-btn');
